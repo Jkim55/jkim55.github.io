65798cb9d74435f930a6683a1eeb74e2e2ffe006
Ji Kim <jkim55@gmail.com>
2016-11-06 00:04:47 -0600

    edited routes

--- js/app.js
+++ js/app.js
@@ -7,16 +7,16 @@ app.config(function($routeProvider, $locationProvider) {
       templateUrl: 'partials/home.html',
       contoller: 'homeController'
     })
-    .when('/bio', {
-      templateUrl: 'partials/bio.html',
+    .when('/about', {
+      templateUrl: 'partials/about.html',
       controller: 'bioController'
     })
     .when('/projects', {
       templateUrl: 'partials/projects.html',
       controller: 'projectController'
     })
-    .when('/resume', {
-      templateUrl: 'partials/resume.html',
+    .when('/contact', {
+      templateUrl: 'partials/contact.html',
       controller: 'resumeController'
     })
     .otherwise({
